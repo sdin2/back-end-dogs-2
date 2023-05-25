8bdad41463a03b84a891efe53c16a68f943d67b0
sdin2 <desat_94@hotmail.com>
2023-05-25 01:22:52 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import { urlencoded, json } from 'body-parser';
-import morgan from 'morgan';
-import routes from './routes/index.js';
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const routes = require('./routes/index.js');
 const {CORS_URL} = process.env
 require('./db.js');
 
@@ -10,8 +10,8 @@ const server = express();
 
 server.name = 'API';
 
-server.use(urlencoded({ extended: true, limit: '50mb' }));
-server.use(json({ limit: '50mb' }));
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
@@ -32,4 +32,4 @@ server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   res.status(status).send(message);
 });
 
-export default server;
+module.exports = server;
