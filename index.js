c730a4cff871f7dcc536facde62b6104abbd5441
sdin2 <desat_94@hotmail.com>
2023-05-25 02:19:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
 const {CORS_URL} = process.env
-require('./db.js');
+// require('./db.js');
 
 const server = express();
 
