ede865c3714b9cd8e53772d6dde40c76d5a37c88
sdin2 <desat_94@hotmail.com>
2023-05-25 02:20:18 -0300

    asd

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const routes = require('./routes/index.js');
-const {CORS_URL} = process.env
-// require('./db.js');
+const server = require('./src/app.js');
+const { conn, Temp } = require('./src/db.js');
+const axios = require("axios");
+const PORT = 5432
 
-const server = express();
+let allTemperamentsDb=async()=>{
+  const temperamentosDb =await axios.get("https://api.thedogapi.com/v1/breeds")
+  let temperamentosDbalone= temperamentosDb.data.map((e)=>e.temperament)
+  let temperament=[]
+      for (let i = 0; i < temperamentosDbalone.length; i++) {
+          if(temperamentosDbalone[i]){
+          temperament.push( temperamentosDbalone[i].split(","))
+      }
+          else continue        
+      }
+  temperament=temperament.flat(Infinity)
+  console.log(temperament)
+  temperament.forEach(el => {
+      Temp.findOrCreate({
+         where: {name : el.trim()}
+     })
+  })
+  console.log("temperamentos agregados a la base de datos")
+  }
 
-server.name = 'API';
-
-// server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-// server.use(bodyParser.json({ limit: '50mb' }));
-// server.use(cookieParser());
-// server.use(morgan('dev'));
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', "*"); // update to match the domain you will make the request from
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
-
-server.use('/', (req,res)=> {
-  res.json({message:"hello"})
+// Syncing all the models at once.
+conn.sync().then(() => {
+  server.listen(PORT, () => {
+    allTemperamentsDb()
+    console.log('%s listening at port',PORT); // eslint-disable-line no-console
+  });
 });
-
-// Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
-});
-
-module.exports = server;
